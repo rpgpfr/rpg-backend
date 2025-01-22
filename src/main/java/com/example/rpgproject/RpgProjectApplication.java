8c2a5797f32af4c79145eead06bf26af05db86a8
Alvin Hamaïde <alvin98mc@gmail.com>
2025-01-22 10:45:47 +0100

    first commit

---
+++ src/main/java/com/example/rpgproject/RpgProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.rpgproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpgProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rpgproject/RpgProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rpgproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
