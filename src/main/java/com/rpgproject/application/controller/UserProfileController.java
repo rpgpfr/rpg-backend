be7e46987b1aa127da40765ab92c52e35afe523b
Alvin Hamaïde <alvin98mc@gmail.com>
2025-03-17 16:45:48 +0100

    feat(user-update): implemented and tested controller method

--- src/main/java/com/rpgproject/application/controller/UserProfileController.java
+++ src/main/java/com/rpgproject/application/controller/UserProfileController.java
@@ -1,35 +1,48 @@
 package com.rpgproject.application.controller;
 
 import com.rpgproject.application.annotation.CurrentOwner;
+import com.rpgproject.application.dto.requestbody.UserUpdateRequestBody;
 import com.rpgproject.application.dto.responsebody.ResponseViewModel;
 import com.rpgproject.application.dto.viewmodel.UserProfileViewModel;
+import com.rpgproject.application.dto.viewmodel.UserViewModel;
 import com.rpgproject.application.presenter.UserProfileRestPresenter;
+import com.rpgproject.application.presenter.UserRestPresenter;
+import com.rpgproject.domain.entity.User;
 import com.rpgproject.domain.port.CampaignRepository;
 import com.rpgproject.domain.port.CharacterRepository;
 import com.rpgproject.domain.port.MapRepository;
 import com.rpgproject.domain.port.UserRepository;
 import com.rpgproject.domain.usecase.GetUserProfile;
+import com.rpgproject.domain.usecase.UpdateUser;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/profile")
 public class UserProfileController {
 
 	private final GetUserProfile<ResponseEntity<ResponseViewModel<UserProfileViewModel>>> getUserProfile;
+	private final UpdateUser<ResponseEntity<ResponseViewModel<UserViewModel>>> updateUser;
 
-	public UserProfileController(UserRepository userRepository, CampaignRepository campaignRepository, MapRepository mapRepository, CharacterRepository characterRepository, UserProfileRestPresenter userProfileRestPresenter) {
+	public UserProfileController(UserRepository userRepository, CampaignRepository campaignRepository, MapRepository mapRepository, CharacterRepository characterRepository, UserProfileRestPresenter userProfileRestPresenter, UserRestPresenter userRestPresenter) {
 		getUserProfile = new GetUserProfile<>(userRepository, campaignRepository, mapRepository, characterRepository, userProfileRestPresenter);
+		updateUser = new UpdateUser<>(userRepository, userRestPresenter);
 	}
 
 	@GetMapping("/")
 	@CrossOrigin(origins = "*")
-	public @ResponseBody ResponseEntity<ResponseViewModel<UserProfileViewModel>> test(@CurrentOwner String username) {
+	public @ResponseBody ResponseEntity<ResponseViewModel<UserProfileViewModel>> getUserProfile(@CurrentOwner String username) {
 		return getUserProfile.execute(username);
+	}
+
+	@PatchMapping("/")
+	@CrossOrigin(origins = "*")
+	public @ResponseBody ResponseEntity<ResponseViewModel<UserViewModel>> update(@CurrentOwner String username, @RequestBody UserUpdateRequestBody userUpdateRequestBody) {
+		User user = new User(username, null, userUpdateRequestBody.firstName(), userUpdateRequestBody.lastName(), null, userUpdateRequestBody.description(), userUpdateRequestBody.rpgKnowledge());
+
+		return updateUser.execute(user);
 	}
 
 }
