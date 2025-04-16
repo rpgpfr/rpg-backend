12465085cb5b9760f6aec95bad5238066d2e3c9b
Alvin Hamaïde <alvin98mc@gmail.com>
2025-04-16 18:34:08 +0200

    refactor(campaign-controller): changed route base

--- src/main/java/com/rpgproject/application/controller/CampaignController.java
+++ src/main/java/com/rpgproject/application/controller/CampaignController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/campaign")
+@RequestMapping("/campaigns")
 @Controller
 public class CampaignController {
 
